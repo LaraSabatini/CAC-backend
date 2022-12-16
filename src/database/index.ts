fa94fe8f1bb30973712281f1bc610aff750e6e79
Lara <sabatinilara@gmail.com>
2022-12-16 13:33:30 -0300

    config: configured environment variables and conections

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import mysql from 'mysql2';
 import config from '../config/index';
 
-const db = mysql.createConnection(config.db);
+const db = mysql.createConnection(config.DB);
 
 export default db;
