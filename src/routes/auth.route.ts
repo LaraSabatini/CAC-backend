3a1700a7df4d0c4a8522c1e23a9bf3504e8c0f0e
Lara <sabatinilara@gmail.com>
2022-12-17 10:39:20 -0300

    trial: trial of dependencie

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { getAll } from '../services/auth.service';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
 router.get('/', getAll);
 
+const users: { user:string, password:string }[] = [];
+
+router.post('/', async (req, res) => {
+  const user = req.body.user_name;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  users.push({ user: user, password: hashedPassword });
+  res.status(201).send(users);
+  console.log(users);
+});
 
 export default router;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -9,4 +9,15 @@ const getAll = async (_req: any, res: any) => {
   }
 };
 
+// const createUser = async (req: any, res: any) => {
+//   try {
+//     const [rows] = await pool.query(`INSERT INTO admin(user_name, email, password)
+//     VALUES ('${req.body.name}','${req.body.user_name}','${req.body.email}','${req.body.password}')`);
+//     res.json(rows);
+//   } catch (error) {
+//     return res.status(500).json({ message: 'Something went wrong' });
+//   }
+// };
+
+
 export { getAll };
