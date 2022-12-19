ffa80f39714dcd97e4dcd6c5d24c7da4d3953a72
Lara <sabatinilara@gmail.com>
2022-12-19 13:58:12 -0300

    config: configured environment to run tests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import config from './config';
 import express from 'express';
 
-const app = express();
-app.use(express.json());
+export const app = express();
 
-console.log(`NODE_ENV=${config.NODE_ENV}`);
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
+// console.log(`NODE_ENV=${config.NODE_ENV}`);
 
 app.get('/', (_req, res) => {
-  res.send('Hello World !!');
+  res.json({ message: 'ok' });
 });
 
 app.listen(config.PORT, () => {
