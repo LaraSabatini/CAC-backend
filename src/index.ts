35f6ba4b573f320de1ea187f108fe51003f22479
Lara <sabatinilara@gmail.com>
2022-12-19 19:18:31 -0300

    WIP: started mercado pago integration

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,100 @@
 import express from "express"
+import mercadopago from "mercadopago"
+import cors from "cors"
 import config from "./config"
 
-import usersRouter from "./routes/auth"
-import paymentsRouter from "./routes/payment"
+// import usersRouter from "./routes/auth"
+// import paymentsRouter from "./routes/payment"
+
+mercadopago.configure({
+  access_token: `${config.MP_ACCESS_TOKEN}`,
+})
 
 const app = express()
 
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use("/users", usersRouter)
-app.use("/payments", paymentsRouter)
+app.use(cors())
 
-app.use(express.json())
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-)
+const preference = {
+  items: [
+    {
+      title: "Mi producto",
+      unit_price: 100,
+      quantity: 1,
+    },
+  ],
+}
 
-app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  )
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
-  next()
-})
+mercadopago.preferences
+  .create(preference)
+  .then(response => {
+    // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
+    // eslint-disable-next-line no-console
+    console.log("MP RES:", response)
+  })
+  .catch(error => {
+    // eslint-disable-next-line no-console
+    console.log("MP ERROR:", error)
+  })
+
+// app.post("/create_preference", (req, res) => {
+//   const preference = {
+//     items: [
+//       {
+//         title: req.body.description,
+//         unit_price: Number(req.body.price),
+//         quantity: Number(req.body.quantity),
+//       },
+//     ],
+//     back_urls: {
+//       success: "http://localhost:8080/feedback",
+//       failure: "http://localhost:8080/feedback",
+//       pending: "http://localhost:8080/feedback",
+//     },
+//     auto_return: "approved",
+//   }
+
+//   mercadopago.preferences
+//     .create(preference)
+//     .then(response => {
+//       res.json({
+//         id: response.body.id,
+//       })
+//     })
+//     .catch(error => {
+//       // eslint-disable-next-line no-console
+//       console.log("MP ERROR:", error)
+//     })
+// })
+
+// app.get("/feedback", (req, res) => {
+//   res.json({
+//     Payment: req.query.payment_id,
+//     Status: req.query.status,
+//     MerchantOrder: req.query.merchant_order_id,
+//   })
+// })
+
+// app.use("/users", usersRouter)
+// app.use("/payments", paymentsRouter)
+
+// app.use(express.json())
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   }),
+// )
+
+// app.use((_req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept",
+//   )
+//   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
+//   next()
+// })
 
 app.get("/", (_req, res) => {
   res.json({ message: "ok" })
--- src/interfaces/Payment.ts
+++ src/interfaces/Payment.ts
@@ -1,4 +1,4 @@
-interface PaymentInterface {
+export interface PaymentInterface {
   id: number
   clientId: number
   mpUser: string
@@ -7,4 +7,54 @@ interface PaymentInterface {
   pricePaid: number
 }
 
-export default PaymentInterface
+export interface PreferenceInterface {
+  items: {
+    id?: string
+    title: string
+    currency_id?: string
+    picture_url?: string
+    description?: string
+    category_id?: string
+    quantity: number
+    unit_price: number
+  }[]
+  payer?: {
+    name: string
+    surname: string
+    email: string
+    phone: {
+      area_code: string
+      number: string
+    }
+    identification: {
+      type: string
+      number: string
+    }
+    address: {
+      street_name: string
+      street_number: number
+      zip_code: string
+    }
+  }
+  back_urls: {
+    success: string
+    failure: string
+    pending: string
+  }
+  auto_return: "approved" | "all" | undefined
+  payment_methods?: {
+    excluded_payment_methods: {
+      id: string
+    }[]
+    excluded_payment_types: {
+      id: string
+    }[]
+    installments: number
+  }
+  notification_url?: string
+  statement_descriptor?: string
+  external_reference?: string
+  expires?: boolean
+  expiration_date_from?: Date
+  expiration_date_to?: Date
+}
