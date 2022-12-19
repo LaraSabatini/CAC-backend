756619f338579c6fb5501b703661e30ed84005d1
Lara <sabatinilara@gmail.com>
2022-12-19 13:37:09 -0300

    feat: created helpers for passwords

---
+++ src/helpers/handleBcrypt.ts
@@ -0,0 +1,12 @@
+import bcrypt from 'bcryptjs';
+
+const encrypt = async (textPlain: string) => {
+  const hash = await bcrypt.hash(textPlain, 10);
+  return hash;
+};
+
+const compare = async (passwordPlain: string, hashPassword: string) => {
+  return bcrypt.compare(passwordPlain, hashPassword);
+};
+
+export { encrypt, compare };
