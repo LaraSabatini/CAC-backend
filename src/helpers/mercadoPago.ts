9d3341be1fac99f574f959515ffb632325fa0318
Lara <sabatinilara@gmail.com>
2022-12-28 12:03:22 -0300

    feat: removed unused stuff

--- src/helpers/mercadoPago.ts
+++
@@ -1,8 +0,0 @@
-import mercadopago from "mercadopago"
-import config from "../config"
-
-mercadopago.configure({
-  access_token: `${config.MP_ACCESS_TOKEN}`,
-})
-
-export default mercadopago
--- src/interfaces/Payment.ts
+++
@@ -1,8 +0,0 @@
-export interface PaymentInterface {
-  id: number
-  clientId: number
-  mpUser: string
-  paymentExpireDate: Date
-  itemId: number
-  pricePaid: number
-}
